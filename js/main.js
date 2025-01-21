efe58481a1cb70dcf48cae860fc5d0f63e030ed5
Ayman Sedkey <aymansedkey10@gamil.com>
2025-01-22 00:23:51 +0200

    fixed the style of the service's cards

--- js/main.js
+++ js/main.js
@@ -142,7 +142,7 @@ async function getData(key) {
                                         </div>
                                         <div class="card-body">
                                             <h3 class="fw-bold text-center p-2 service-title">${link.title}</h3>
-                                            <h4 class="card-text fw-semibold">${link.details}</h4>
+                                            <p class="card-text fw-semibold">${link.details}</p>
                                         </div>
                                     </div>
                                 </div>`;
