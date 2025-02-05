d83d8d50bcac14b4fa8777d6cef390863a8275af
Ayman Sedkey <aymansedkey10@gamil.com>
2025-02-05 04:46:53 +0200

    upload the changes after add pages to project

--- js/main.js
+++ js/main.js
@@ -1,62 +1,64 @@
 let imagesUrl = [
-  "./images/s-1.jpeg",
-  "./images/s-2.jpeg",
-  "./images/s-3.jpeg",
-  "./images/s-4.jpeg",
+  "../images/s-1.jpeg",
+  "../images/s-2.jpeg",
+  "../images/s-3.jpeg",
+  "../images/s-4.jpeg",
 ];
 let currentIndex = 0;
 
-window.addEventListener("load", () => {
-  let butttonToShowConettionButtons = document.getElementById(
-    "butttonToShowConettionButtons"
-  );
+// call function for show data
+getData("navBar");
+getData("aboutUs");
+getData("services");
+getData("providers");
+
+window.addEventListener("DOMContentLoaded", () => {
+  let butttonToShowConettionButtons = document.getElementById("butttonToShowConettionButtons");
+  // change the color of the buttons connections by data-color
   changeColorConnectionsButton();
+  // change the background of header
   changeBackground();
   setInterval(changeBackground, 1500);
+  // change the dispaly of the buttons connections when click
+  butttonToShowConettionButtons.addEventListener("click", showConnectionButtons);
 
-  butttonToShowConettionButtons.addEventListener(
-    "click",
-    showConnectionButtons
-  );
-
-  window.addEventListener("scroll", () => {
-    showElements();
-    navBarFixed();
-  });
-
-  // call function for show data
-  getData("navBar");
-  getData("aboutUs");
-  getData("services");
-  getData("providers");
+  // call scroll to down when change the Url
+  scrollToDown();
 });
 
+window.addEventListener("scroll", () => {
+  showElements();
+  navBarFixed();
+});
 // funtion for get data from json
 async function getData(key) {
-  let response = await fetch("./data/data.json");
+  let response = await fetch("../data/data.json");
   let data = await response.json();
 
   if (data !== null && data.status === true) {
     if (key === "navBar") {
       let navBar = document.getElementById("nav-bar-list");
-      data[key].forEach((link) => {
-        let itemLink = `<li class="nav-item">
-                                    <a class="nav-link fw-bold text-white" href="${link.link}">${link.title}</a>
-                                </li>`;
-        navBar.innerHTML += itemLink;
-      });
+      if (navBar) {
+
+        data[key].forEach((link) => {
+          let itemLink = `<li class="nav-item">
+                                            <a class="nav-link fw-bold text-white" href="${link.link}">${link.title}</a>
+                                        </li>`;
+          navBar.innerHTML += itemLink;
+        });
+      }
     }
     if (key === "aboutUs") {
       let aboutUs = document.getElementById("controllers");
+      if (aboutUs) {
+        data[key].forEach((link, index) => {
+          let uniqueId = `section-${index}`;
+          let aboutContentFromJson = link.details;
+          let itemLink = "";
 
-      data[key].forEach((link, index) => {
-        let uniqueId = `section-${index}`;
-        let aboutContentFromJson = link.details;
-        let itemLink = "";
-
-        // Check if details is not an array
-        if (!Array.isArray(aboutContentFromJson)) {
-          itemLink = `
+          // Check if details is not an array
+          if (!Array.isArray(aboutContentFromJson)) {
+            itemLink = `
                         <div class="active-content rounded mb-2">
                             <div class="d-flex align-items-center justify-content-between p-2 controller-container">
                                 <button class="border-0 bg-transparent fw-bold w-100 text-end controller-button" data-target="${uniqueId}">
@@ -68,17 +70,17 @@ async function getData(key) {
                                 <p>${aboutContentFromJson}</p>
                             </div>
                         </div>`;
-          aboutUs.innerHTML += itemLink;
-        } else {
-          let listItems = aboutContentFromJson
-            .map((element) => {
-              return element.content
-                ? `<li>${element.content}</li>`
-                : `<li>Content not available</li>`;
-            })
-            .join("");
+            aboutUs.innerHTML += itemLink;
+          } else {
+            let listItems = aboutContentFromJson
+              .map((element) => {
+                return element.content
+                  ? `<li>${element.content}</li>`
+                  : `<li>Content not available</li>`;
+              })
+              .join("");
 
-          itemLink = `
+            itemLink = `
                         <div class="active-content rounded mb-2">
                             <div class="d-flex align-items-center justify-content-between p-2 controller-container">
                                 <button class="border-0 bg-transparent fw-bold w-100 text-end controller-button" data-target="${uniqueId}">
@@ -90,9 +92,10 @@ async function getData(key) {
                                 <ul class="m-0 px-3">${listItems}</ul>
                             </div>
                         </div>`;
-          aboutUs.innerHTML += itemLink;
-        }
-      });
+            aboutUs.innerHTML += itemLink;
+          }
+        });
+      }
       // add event to show content
       let buttons = document.querySelectorAll(".controller-button");
       let contents = document.querySelectorAll(".content-area");
@@ -135,38 +138,44 @@ async function getData(key) {
     }
     if (key === "services") {
       let services = document.getElementById("services");
-      data[key].forEach((link) => {
-        let itemLink = `<div class="col-12 col-md-6 col-lg-4 mb-4">
+      if (services) {
+        data[key].forEach((service) => {
+          let itemService = `<div class="col-12 col-md-6 col-lg-4 mb-4">
                                     <div class="card overflow-hidden position-relative">
                                         <div class="overflow-hidden">
-                                            <img src="${link.image}" class="card-img-top" alt="${link.title}">
+                                            <img src="${service.image}" class="card-img-top" alt="${service.title}">
                                         </div>
                                         <div class="card-body">
-                                            <h3 class="fw-bold text-center p-2 service-title">${link.title}</h3>
-                                            <h6 class="card-text fw-semibold">${link.details}</h6>
+                                            <h3 class="fw-bold text-center p-2 service-title">${service.title}</h3>
+                                            <h6 class="card-text fw-semibold">${service.details}</h6>
                                         </div>
                                     </div>
                                 </div>`;
-        services.innerHTML += itemLink;
-      });
+          services.innerHTML += itemService;
+        });
+      }
     }
     if (key === "providers") {
       let providers = document.getElementById("providers");
-      data[key].forEach((link) => {
-        let itemLink = `<div class="col-12 col-md-6 col-lg-4 mb-4">
+      if (providers) {
+        data[key].forEach((Provider) => {
+          let itemProvider = `<div class="col-12 col-md-6 col-lg-4 mb-4">
                                 <div class="card border overflow-hidden">
                                     <div class="overflow-hidden">
-                                        <img src="${link.image}" class="card-img-top" alt="${link.title}">
+                                        <img src="${Provider.image}" class="card-img-top" alt="${Provider.title}">
                                     </div>
                                     <div class="card-body">
-                                    <h3 class="fw-bold text-center">${link.title}</h3>
-                                    <p class="card-text fw-semibold">${link.details}</p>
+                                    <h3 class="fw-bold text-center">${Provider.title}</h3>
+                                    <p class="card-text fw-semibold">${Provider.details}</p>
                                     </div>
                                 </div>
                                 </div>`;
-        providers.innerHTML += itemLink;
-      });
+          providers.innerHTML += itemProvider;
+        });
+      }
     }
+  } else {
+    console.log("data not found");
   }
 }
 // function for fixed navbar
@@ -187,12 +196,12 @@ function navBarFixed() {
     });
   }
 }
-// function for change background image
+// function for change background image of header
 function changeBackground() {
   let container = document.getElementById("content-header");
   container.style.backgroundImage = `url(${imagesUrl[currentIndex]})`;
   currentIndex = (currentIndex + 1) % imagesUrl.length;
- 
+
 }
 
 // show elements when scroll
@@ -225,38 +234,66 @@ function showConnectionButtons() {
     button.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
   }
 }
-// change the color 
-function changeColorConnectionsButton(){
+// change the color of the buttons connections by data-color
+function changeColorConnectionsButton() {
   let conettionButtons = document.querySelectorAll("#button-connection");
-  conettionButtons.forEach((button)=>{
+  conettionButtons.forEach((button) => {
     let color = button.getAttribute("data-color");
     button.style.backgroundColor = color;
   })
 }
-// Disable right-click
-document.addEventListener("contextmenu", (event) => {
-  event.preventDefault();
-});
 
-// Disable key combinations (like F12, Ctrl+Shift+I, etc.)
-document.addEventListener("keydown", (event) => {
-  // Disable F12
-  if (event.key === "F12") {
-    event.preventDefault();
+// function to scroll when the Url is changed 
+function scrollToDown() {
+  const Url_link = window.location.href.split("/").pop();
+  if (Url_link) {
+    setTimeout(() => { window.scrollTo({ top: 500, behavior: "smooth" }); }, 500);
   }
+}
 
-  // Disable Ctrl+Shift+I (Windows/Linux) or Cmd+Option+I (Mac)
-  if ((event.ctrlKey || event.metaKey) && event.shiftKey && event.key === "I") {
-    event.preventDefault();
-  }
+// // Disable right-click
+// document.addEventListener("contextmenu", (event) => {
+//   event.preventDefault();
+// });
 
-  // Disable Ctrl+U (View Source)
-  if ((event.ctrlKey || event.metaKey) && event.key === "u") {
-    event.preventDefault();
-  }
+// // Disable key combinations (like F12, Ctrl+Shift+I, etc.)
+// document.addEventListener("keydown", (event) => {
+//   // Disable F12
+//   if (event.key === "F12") {
+//     event.preventDefault();
+//   }
 
-  // Disable Ctrl+Shift+J (Console)
-  if ((event.ctrlKey || event.metaKey) && event.shiftKey && event.key === "J") {
-    event.preventDefault();
-  }
-});
+//   // Disable Ctrl+Shift+I (Windows/Linux) or Cmd+Option+I (Mac)
+//   if ((event.ctrlKey || event.metaKey) && event.shiftKey && event.key === "I") {
+//     event.preventDefault();
+//   }
+
+//   // Disable Ctrl+U (View Source)
+//   if ((event.ctrlKey || event.metaKey) && event.key === "u") {
+//     event.preventDefault();
+//   }
+
+//   // Disable Ctrl+Shift+J (Console)
+//   if ((event.ctrlKey || event.metaKey) && event.shiftKey && event.key === "J") {
+//     event.preventDefault();
+//   }
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let links = document.querySelectorAll("nav a"); // تحديد كل الروابط داخل القائمة
+//   let currentUrl = window.location.pathname.split("/").pop(); // جلب اسم الصفحة الحالية
+
+//   links.forEach(link => {
+//       if (link.getAttribute("href") === currentUrl) {
+//           link.classList.add("active"); // إضافة كلاس active للرابط الخاص بالصفحة الحالية
+//       }
+//   });
+// // });
+
+// let links = document.querySelectorAll("nav a");
+// let currentUrl = window.location.pathname.split('/').pop();
+// links.forEach(link => {
+//   if (link.getAttribute("href") === currentUrl) {
+//     link.classList.toggle("active-linkactive");
+//   }
+// })
